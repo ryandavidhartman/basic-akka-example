c1bafbd9c0bf1cdc8530af7cf4a382bbe871168f
Ryan Hartman <ryandavidhartman@gmail.com>
2022-12-10 00:26:21 -0500

    clean up of the recap chapter

--- akka-essentials/src/main/scala/part1recap/GeneralRecap.scala
+++ akka-essentials/src/main/scala/part1recap/GeneralRecap.scala
@@ -1,34 +1,42 @@
 package part1recap
 
 object GeneralRecap extends App {
-  val aCondition: Boolean = false //how to declare an immutable val
-  var aVariable = 1  //how to declare a variable, reassignment is allowed
+  private val aCondition: Boolean = false //how to declare an immutable val
+  private var aVariable = 1  //how to declare a variable, reassignment is allowed
   aVariable = 2
 
   // expressions
-  val aConditionalVal = if(aCondition) 42 else 65
+  private val aConditionalVal: Int = if(aCondition) 42 else 65
+  assert(aConditionalVal == 65)
 
   // code block
-  val aCodeBlock = {
+  private val aCodeBlock = {
     if(aCondition) 74
     56  // this is the value returned by the expression that is the code block
   }
 
+  assert(aCodeBlock == 56)
+
   //types
   // Unit
-  val theUnit:Unit = println("Hello, Scala") //Unit is the type of something that just has side effects
+  private val theUnit: Unit = println("Hello, Scala") //Unit is the type of something that just has side effects
+  theUnit
 
-  def aFunction(x:Int): Int = x + 1
-  val aAnotherFunction: (Int) => Int = { x => x+1}
+  private def aFunction(x: Int): Int = x + 1
+  assert(aFunction(4) == 5)
+  assert(aFunction(9) == 10)
 
-  val test = aAnotherFunction(5)
+  private val aAnotherFunction: Int => Int = { x => x+1}
+
+  private val test = aAnotherFunction(5)
   println(test)
 
   //recursion & tail recursion
 
   @scala.annotation.tailrec
-  def factorial(n:Int, acc:Int = 1): Int =
+  private def factorial(n:Int, acc:Int = 1): Int =
     if(n <= 0) acc
     else factorial(n-1, acc*n)
 
+  assert(factorial(3) == 6)
 }
