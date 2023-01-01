77658ea32525eed1ab5c7aa2094273ac550a6e76
ryandavidhartman <ryandavidhartman@gmail.com>
2022-12-31 21:01:59 -0500

    first actors

--- akka-essentials/src/main/scala/part2actors/ActorsIntro.scala
+++ akka-essentials/src/main/scala/part2actors/ActorsIntro.scala
@@ -1,5 +1,24 @@
 package part2actors
 
+import akka.actor.ActorSystem
+
 object ActorsIntro extends App {
+
+  // part 1 actor systems
+  val actorSystem = ActorSystem("firstActorSystem")  // Note: name must contain only word characters
+                                                     // (i.e. [a-zA-Z0-9] plus non-leading '-' or '_')
+  println("Actor system: " + actorSystem.name + " started at: " +  actorSystem.startTime)
+
+  // part 2 create some actors
+
+  /*
+    1) Actors are uniquely identified withing an actor
+    2) Messages are sent and processes asynchronously
+    3) Each actor has maybe respond differently to a given message
+    4) Actors are encapsulated so that the only interaction you may have with them is by message passing.
+  */
+
+
+
 
 }
